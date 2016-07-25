b4fef34c094ab0ece211075ff936d07edd3e7339
OSCAR GARCIA-TELLES <oscargt@uw1-320-01.uwb.edu>
2016-07-25 15:38:16 -0700

    updated Superblock.java to compile

--- Superblock.java
+++ Superblock.java
@@ -5,14 +5,17 @@
 //manages the list of free blocks
 public class Superblock
 {
-    public int totalBlcoks; //the number of disk blcoks
+    public int totalBlocks; //the number of disk blcoks
     public int totalInodes; //the number of inodes
     public int freeList; // the block number of the free list's head
 
     //read the superblock from disk
     public Superblock(int diskSize)
     {
-
+    		// Place holder values just to compile!
+		totalBlocks = 0;
+		totalInodes = 10;
+		freeList = 0;
     }
 
 }
