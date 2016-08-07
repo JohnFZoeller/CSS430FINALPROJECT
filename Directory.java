85210fef51d8e19e8da0e8c3a1df83ac0dea0f60
Oscar <oscar.garcia_telles@hotmail.com>
2016-08-07 16:38:12 -0700

    More methods filled in

--- Directory.java
+++ Directory.java
@@ -83,6 +83,8 @@ public class Directory
     	}
     	catch(ArrayIndexOutOfBoundsException e)
     	{
+    		SysLib.cerr("Exception with accessing byte array in Directory.java. \n");
+    		e.printStackTrace();
     		return -1;
     	}
     	
@@ -97,17 +99,72 @@ public class Directory
     // into bytes.
     public byte[] directory2bytes( )
     {
-        byte[] temp = new byte[20];
+    	int intBytes = fsize.length * 4;
+    	int charBytes = fnames.length * maxChars * 2;
+        byte[] temp = new byte[intBytes + charBytes];
+        
+        // Populating byte array with file size int data
+        int offset = 0;
+        int currentInt = 0;
+        for(int i = 0; i < fsize.length; i++)
+        {
+        	currentInt = fsize[i];
+        	SysLib.int2bytes(currentInt, temp, offset);
+        	offset = offset + 4; // 4 Bytes per int
+        }
+        
+        // Now populating byte array with fname char data
+        // Outer loop will traverse each name
+        for(int i = 0; i < fnames.length; i ++)
+        {
+        	// Retrieving current fname and trimming non chars
+        	String currStr = new String(fnames[i], 0, fsize[i]);
+        	// Making byte array of current string
+        	byte currentByteArr[] = currStr.getBytes();
+        	// Now storing bytes into overall byte array
+        	System.arraycopy(currentByteArr, 0, temp, offset, currentByteArr.length);
+        	// Could have stored 30 chars * 2Bytes/char = 60 bytes
+        	offset = offset + (maxChars * 2);
+        }
         return temp;
     }
 
     // ------------- ialloc(String filename) -------------
     
-    // filename is the one of a file to be created.
-    // allocates a new inode number for this filename
+    // Filename is the one of a file to be created.
+    // allocates a new inode number for this filename.
     public short ialloc( String filename )
     {
-        return 0;
+    	short result = (short) -1;
+    	int nameLength = filename.length();
+    	if(nameLength == 0)
+    	{
+    		SysLib.cerr("Error with ialloc(String filename) in Directory: filename.length() = 0 \n");
+    		return result;
+    	}
+    	
+    	// Will need to see if there's a slot that's not used
+    	for(int i = 1; i < fsize.length; i++)
+    	{
+    		// Checking if not used
+    		if(fsize[i] == 0)
+    		{
+    			result = (short) i;
+    			// Checking if filename is too long
+    			if(nameLength > maxChars)
+    			{
+    				fsize[i] = maxChars;
+    			}
+    			else
+    			{
+    				fsize[i] = nameLength;
+    			}
+    			
+    			// Now storing filename in fnames[][] array
+    			filename.getChars( 0, fsize[i], fnames[i], 0 ); 
+    		}
+    	}
+        return result;
     }
 
     // ------------- ifree(short iNumber) --------------
@@ -116,13 +173,23 @@ public class Directory
     // the corresponding file will be deleted.
     public boolean ifree( short iNumber )
     {
-
-        /*for(int i = 0; i < fsize[iNumber]; i++)
+    	short lowest = (short) 1; // Cannot deallocate root directory
+    	short greatest = (short) fsize.length;
+    	// Checking if out of bounds
+    	if(iNumber < lowest || iNumber >= greatest)
+    	{
+    		SysLib.cerr("Error in ifree(short iNumber) in Directory: iNumber out of bounds \n");
+    		return false;
+    	}
+    	
+    	// Clearing the filename, replacing chars with
+    	// white space.
+        for(int i = 0; i < fsize[iNumber]; i++)
         {
-            set fnames[iNumber][i] to 0;
+            fnames[iNumber][i] = ' ';
         }
-        set fsize[iNumber] to 0;
-        */
+        fsize[iNumber] = 0;
+        
         return true;
     }
 
